53ce206187e4c1ed7f9a2745f8b03a295b2357d8
yangdehong <405104787@qq.com>
2021-05-27 18:39:17 +0800

    去掉一些不关注的内容

--- src/main/java/com/ydh/redsheep/Test.java
+++ src/main/java/com/ydh/redsheep/Test.java
@@ -1,21 +1,8 @@
 package com.ydh.redsheep;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Test {
 
     public static void main(String[] args) {
-        String str = "0001";
-        Pattern p = Pattern.compile("(\\d+)");
-        Matcher m = p.matcher(str);
-        if (m.find()) {
-            System.out.println(m.group());
-            System.out.println(m.start());
-            System.out.println(m.end());
-            String[] split = str.split(m.group());
-            System.out.println(split.length);
-        }
 
     }
 
